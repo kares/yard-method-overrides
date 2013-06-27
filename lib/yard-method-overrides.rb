7b4967860c997920ab6184a0bab7aa6b68f195a1
kares <self@kares.org>
2013-06-27 11:55:58 +0200

    fix template path resolution for all ruby versions

--- lib/yard-method-overrides.rb
+++ lib/yard-method-overrides.rb
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 require 'yard-method-overrides/version'
 
-YARD::Templates::Engine.register_template_path File.expand_path('../../templates', __FILE__)
+YARD::Templates::Engine.register_template_path File.expand_path('../templates', File.dirname(__FILE__))
 YARD::Tags::Library.define_tag 'Extending Method', :extension
 YARD::Tags::Library.define_tag 'Overriding Method', :override
